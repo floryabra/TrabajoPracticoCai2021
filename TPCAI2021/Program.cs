ad002b11dd0b9f8c739031f846c998727ac1fcf7
florencia.yabra <florencia.yabra@avature.net>
2021-11-01 19:28:57 -0300

    Agregar algunas validaciones en pseudo codigo

--- TPCAI2021/Program.cs
+++ TPCAI2021/Program.cs
@@ -10,12 +10,56 @@ namespace TPCAI2021
     {
         static void Main(string[] args)
         {
-
-
+            int idClienteCorporativo = IngresarClienteCorporativo();
+            if(idClienteCorporativo != -1) 
+            {
+                MostrarMenuClienteCorporativo(idClienteCorporativo);
+            }
             var cuenta = new CuentaCorriente(123, 123, (decimal)10.0, "test");
             var cliente = new Cliente(123, cuenta, "test");
             Console.WriteLine("Se creó un cliente por defecto con el nro de cuenta: " + cliente.nroClienteCorporativo);
             Console.ReadLine();
+        }
+
+        static void IngresarClienteCorporativo() {
+            // ingresar y validar cliente corpo
+            // return true si esta ok
+            return -1;
+        }
+
+        static void MostrarMenuClienteCorporativo(int idClienteCorporativo) {
+            // Mostrar las opciones de menu y llamar a la función de la option elegida
+            switch (variableOpcionDeMenu)
+            {
+                case 1:
+                    RealizarSolicitudDeServicio(idClienteCorporativo);
+                break;
+
+                case 2:
+                    ConsultarEstadoOrdenDeServicio(idClienteCorporativo);
+                break;
+
+                case 3:
+                    ConsultarEstadoDeCuenta(idClienteCorporativo);
+                break;
+
+                default:
+            }
+        }
+
+        static void RealizarSolicitudDeServicio(int idClienteCorporativo) {
+            // Pedir, validar datos y armar tarifa
+            // Aprobar orden de servicio y guardar
+        }
+
+        static void ConsultarEstadoOrdenDeServicio(int idClienteCorporativo) {
+            // pedir id orden de servicio y mostrar el status
+            // OrdenServicio::conocerEstadoOrdenServicio
+        }
+
+        static void ConsultarEstadoDeCuenta(int idClienteCorporativo) {
+            // mostrar status de cuenta cte.
+            // CuentaCorriente::conocerEstadoCuentaCorriente();
         }
     }
 }
