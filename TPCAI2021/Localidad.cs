fbd0b8934f04e8ed6bad8a7725eede4550ebc357
niquito <ncaffarini@gmail.com>
2021-11-08 23:12:44 -0300

    Create Localidad.cs

---
+++ TPCAI2021/Localidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPCAI2021
+{
+    [Table("Localidades")]
+    class Localidad
+    {
+        public int LocalidadID { get; set; }
+        public string Nombre { get; set; }
+        public Provincia Provincia { get; set; }
+        public ICollection<Sucursal> Sucursales { get; set; }
+    }
+}
