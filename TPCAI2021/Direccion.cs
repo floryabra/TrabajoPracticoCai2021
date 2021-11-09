1c167f464ac31b9a6455d2de94b2e63a353f9de8
niquito <ncaffarini@gmail.com>
2021-11-09 12:22:59 -0300

    Update Direccion.cs

--- TPCAI2021/Direccion.cs
+++ TPCAI2021/Direccion.cs
@@ -4,24 +4,45 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TPCAI2021.data;
 
 namespace TPCAI2021
 {
     [Table("Direcciones")]
     class Direccion
     {
+        private string piso;
+
         public int DireccionId { get; set; }
         public bool AlcanceNacional { get; set; }
-        public string Provincia { get; set; }
-        public string Ciudad { get; set; }
         public string Calle { get; set; }
+        public string Altura { get; set; }
         public int CodigoPostal { get; set; }
-        public int? Piso { get; set; }
-        public char? Depto { get; set; }
+        public string Piso { get; set; }
+        public string Depto { get; set; }
+        public Localidad Localidad { get; set; }
 
-        public string ingresarDireccion()
+        public static Direccion ingresarDireccion(int idLocalidad, bool alcance = true)
         {
-            return "test ingresarDireccion";
+            var ctx = new TPContext();
+
+            Console.WriteLine("Ingrese la calle:");
+            string calle = Console.ReadLine();
+            Console.WriteLine("Ingrese la altura:");
+            string altura = Console.ReadLine();
+            Console.WriteLine("Ingrese el CP:");
+            int codigoPostal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el piso, si corresponde:");
+            string piso = Console.ReadLine();
+            Console.WriteLine("Ingrese el departamento, si corresponde:");
+            string depto = Console.ReadLine();
+
+            Localidad localidad = ctx.Localidades.Find(idLocalidad);
+
+            var direccion = new Direccion() { AlcanceNacional = alcance, Calle = calle, Altura = altura, CodigoPostal = codigoPostal, Piso = piso, Depto = depto, Localidad = localidad };
+            ctx.Direcciones.Add(direccion);
+            ctx.SaveChanges();
+            return direccion;
         }
     }
 }
