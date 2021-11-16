8fdf70ea01f426ad6e8452a83a95f66877659eed
BelenPas <89458871+BelenPas@users.noreply.github.com>
2021-11-16 08:24:07 -0300

    Update Direccion.cs

--- TPCAI2021/Direccion.cs
+++ TPCAI2021/Direccion.cs
@@ -17,27 +17,27 @@ namespace TPCAI2021
         public string Altura { get; set; }
         public int CodigoPostal { get; set; }
         public string Piso { get; set; }
-        public string Depto { get; set; }
+       // public string Depto { get; set; }
         public Localidad Localidad { get; set; }
 
         public static Direccion ingresarDireccion(int idLocalidad, bool alcance = true)
         {
             var ctx = new TPContext();
 
-            Console.WriteLine("Ingrese la calle:");
+            Console.WriteLine("Ingrese el nombre de la calle:");
             string calle = Console.ReadLine();
-            Console.WriteLine("Ingrese la altura:");
+            Console.WriteLine("Ingrese la altura:"); //Validar que sean numeros
             string altura = Console.ReadLine();
-            Console.WriteLine("Ingrese el CP:");
+            Console.WriteLine("Ingrese el Código Postal:");
             int codigoPostal = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese el piso, si corresponde:");
-            string piso = Console.ReadLine();
-            Console.WriteLine("Ingrese el departamento, si corresponde:");
-            string depto = Console.ReadLine();
+            Console.WriteLine("Ingrese Piso y letra del departamento(En caso de que no corresponda, déjelo nulo):");
+            string piso = Console.ReadLine(); //En el mismo string va a agregar piso y depto
+           // Console.WriteLine("Ingrese el departamento, si corresponde:");
+           // string depto = Console.ReadLine();
 
             Localidad localidad = ctx.Localidades.Find(idLocalidad);
 
-            var direccion = new Direccion() { AlcanceNacional = alcance, Calle = calle, Altura = altura, CodigoPostal = codigoPostal, Piso = piso, Depto = depto, Localidad = localidad };
+            var direccion = new Direccion() { AlcanceNacional = alcance, Calle = calle, Altura = altura, CodigoPostal = codigoPostal, Piso = piso,/* Depto = depto,*/ Localidad = localidad };
             ctx.Direcciones.Add(direccion);
             ctx.SaveChanges();
             return direccion;
